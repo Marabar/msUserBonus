5bceef4e0ed4c419a706a0d23d9af94fc094e5f9
Marabar <marat@marabar.ru>
2016-10-09 12:06:20 +0500

    Закоментирован чанк

--- _build/properties/properties.msuserbonus.php
+++ _build/properties/properties.msuserbonus.php
@@ -3,34 +3,10 @@
 $properties = array();
 
 $tmp = array(
-    'tpl' => array(
+    /*'tpl' => array(
         'type' => 'textfield',
         'value' => 'tpl.msUserBonus.item',
-    ),
-    'sortby' => array(
-        'type' => 'textfield',
-        'value' => 'name',
-    ),
-    'sortdir' => array(
-        'type' => 'list',
-        'options' => array(
-            array('text' => 'ASC', 'value' => 'ASC'),
-            array('text' => 'DESC', 'value' => 'DESC'),
-        ),
-        'value' => 'ASC',
-    ),
-    'limit' => array(
-        'type' => 'numberfield',
-        'value' => 10,
-    ),
-    'outputSeparator' => array(
-        'type' => 'textfield',
-        'value' => "\n",
-    ),
-    'toPlaceholder' => array(
-        'type' => 'combo-boolean',
-        'value' => false,
-    ),
+    ),*/
 );
 
 foreach ($tmp as $k => $v) {
